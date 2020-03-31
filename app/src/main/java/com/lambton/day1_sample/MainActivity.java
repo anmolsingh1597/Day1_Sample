96913ca0f3539a38c553819950eca8e43ef30eca
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-30 20:48:27 -0400

    [Anmol Singh]Login Design

--- app/src/main/java/com/lambton/day1_sample/MainActivity.java
+++ app/src/main/java/com/lambton/day1_sample/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public EditText enterUserName;
     public EditText enterPass;
     private Button btnClickMe;
     private TextView setLabel;
@@ -21,22 +22,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enterUserName = findViewById(R.id.enterUserName);
         enterPass = findViewById(R.id.enterPassword);
         btnClickMe = findViewById(R.id.btnClickMe);
         setLabel = findViewById(R.id.dynamicText);
-
 
         btnClickMe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"Done",Toast.LENGTH_SHORT).show();
 
-                String name = enterPass.getText().toString();
-                Log.d("NAME", name);
+                String userName = enterUserName.getText().toString();
+                String password = enterPass.getText().toString();
 
-                setLabel.setText(name);
+                if (userName.equals("admin@123") && password.equals("s3cr3t"))
+                {
+                    System.out.println("If");
+                    Log.d("NAME", password);
 
+                    setLabel.setText("Login Successfull");
+                    setLabel.setTextColor(getResources().getColor(R.color.Green));
 
+                }else {
+                    System.out.println("Else");
+                    setLabel.setText("Login Failed");
+                    setLabel.setTextColor(getResources().getColor(R.color.Red));
+                }
 
             }
         });
